391c92bee27c40ad6decc7994b6c2ea7c1820213
indra <indra494@gmail.com>
2023-10-23 09:39:25 +0900

    first commit

---
+++ src/test/java/com/indra/discoveryservice/AssignSpringMsaBoardDiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indra.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignSpringMsaBoardDiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
